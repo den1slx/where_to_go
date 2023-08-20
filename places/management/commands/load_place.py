569084da0e5abbd11b8b1aab115333c8d1ed3e95
den1slx <keycoingold@mail.ru>
2023-08-21 01:44:34 +1000

    Update load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -26,55 +26,65 @@ class Command(BaseCommand):
     '''
 
     def handle(self, *args, **options):
-        with open(options['path'], 'r', encoding='UTF-8') as file:
-            place = json.load(file)
-            filtered_place = Place.objects.filter(
+        if options['u']:
+            response = requests.get(options['path'])
+            place = response.json()
+        else:
+            with open(options['path'], 'r', encoding='UTF-8') as file:
+                place = json.load(file)
+        filtered_place = Place.objects.filter(
+            lat=place['coordinates']['lat'],
+            lng=place['coordinates']['lng'],
+            title=place['title'],
+        )
+        if len(filtered_place) > 1:
+            pass
+        elif not filtered_place:
+            Place.objects.create(
                 lat=place['coordinates']['lat'],
                 lng=place['coordinates']['lng'],
                 title=place['title'],
+                description_short=place['description_short'],
+                description_long=place['description_long'],
             )
-            if len(filtered_place) > 1:
-                pass
-            elif not filtered_place:
-                Place.objects.create(
-                    lat=place['coordinates']['lat'],
-                    lng=place['coordinates']['lng'],
-                    title=place['title'],
-                    description_short=place['description_short'],
-                    description_long=place['description_long'],
-                )
-                created_place = Place.objects.filter(
-                    lat=place['coordinates']['lat'],
-                    lng=place['coordinates']['lng'],
-                    title=place['title'],
-                    description_short=place['description_short'],
-                    description_long=place['description_long'],
-                ).first()
-                for img in place['imgs']:
-                    response = requests.get(img)
-                    response.raise_for_status()
-                    img = response.content
-                    img = ContentFile(img)
-                    place_image = PlaceImage.objects.create(place=created_place)
-                    place_image.image.save(f'{created_place.id}{place_image.id}.png', img)
-            else:
-                place_obj = filtered_place.first()
-                place_obj.lat = place['coordinates']['lat'],
-                place_obj.lng = place['coordinates']['lng'],
-                place_obj.title = place['title'],
-                place_obj.description_short = place['description_short'],
-                place_obj.description_long = place['description_long']
-                # TODO avoid doubling images
-                for img in place['imgs']:
-                    response = requests.get(img)
-                    response.raise_for_status()
-                    img = response.content
-                    img = ContentFile(img)
-                    place_image = PlaceImage.objects.create(place=place_obj)
-                    place_image.image.save(f'{place_obj.id}{place_image.id}.png', img)
+            created_place = Place.objects.filter(
+                lat=place['coordinates']['lat'],
+                lng=place['coordinates']['lng'],
+                title=place['title'],
+                description_short=place['description_short'],
+                description_long=place['description_long'],
+            ).first()
+            for img in place['imgs']:
+                response = requests.get(img)
+                response.raise_for_status()
+                img = response.content
+                img = ContentFile(img)
+                place_image = PlaceImage.objects.create(place=created_place)
+                place_image.image.save(f'{created_place.id}{place_image.id}.png', img)
+        else:
+            place_obj = filtered_place.first()
+            place_obj.lat = place['coordinates']['lat'],
+            place_obj.lng = place['coordinates']['lng'],
+            place_obj.title = place['title'],
+            place_obj.description_short = place['description_short'],
+            place_obj.description_long = place['description_long']
+            # TODO avoid doubling images
+            for img in place['imgs']:
+                response = requests.get(img)
+                response.raise_for_status()
+                img = response.content
+                img = ContentFile(img)
+                place_image = PlaceImage.objects.create(place=place_obj)
+                place_image.image.save(f'{place_obj.id}{place_image.id}.png', img)
 
     def add_arguments(self, parser):
         parser.add_argument(
             'path',
             help='path to json file'
+        )
+        parser.add_argument(
+            '-u',
+            '-url',
+            action='store_true',
+            help='For url path'
         )
