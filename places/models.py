413b041988157e428111410f16d1de8223d53f03
den1slx <keycoingold@mail.ru>
2023-08-30 20:05:21 +1000

    Update models.py

--- places/models.py
+++ places/models.py
@@ -5,33 +5,32 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField('Название', max_length=200, null=True)
-    description_short = models.CharField('краткое описание', null=True, max_length=350)
-    lng = models.FloatField('Долгота', null=True)
-    lat = models.FloatField('Широта', null=True)
-    description_long = HTMLField('Описание', null=True, blank=True)
+    title = models.CharField('Название', max_length=200)
+    description_short = models.TextField('Краткое описание', default='')
+    lng = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
+    description_long = HTMLField('Описание', default='', blank=True)
 
     def __str__(self):
         return self.title
 
 
 class PlaceImage(models.Model):
-    image = models.ImageField(upload_to='images/')
-    place = models.ForeignKey(Place, null=True, on_delete=models.CASCADE, related_name='images')
-    my_order = models.PositiveIntegerField(
+    image = models.ImageField('Картинка', upload_to='images/')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name='Место')
+    ordering = models.PositiveIntegerField(
         default=0,
-        blank=False,
-        null=False,
         db_index=True,
         editable=True,
-        unique=False,
+        blank=True,
+        verbose_name='Упорядочение'
     )
 
     def __str__(self):
         return f'{self.place.title} image{self.place.id}{self.id}'
 
     class Meta:
-        ordering = ['my_order',]
+        ordering = ['ordering',]
         verbose_name_plural = "Images"
 
 
